f827ce32c28779aa67f824dc7c07618044281fb7
Patricio Quintanilla <patricioquintanilladx@gmail.com>
2024-09-16 18:56:05 -0300

    se agrega la opcion de portafolio

--- CentroComando/paginaweb/admin.py
+++ CentroComando/paginaweb/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import SolicitudServicio
+from django.contrib import admin
+from .models import PortfolioProject
 
 # Register your models here.
 @admin.register(SolicitudServicio)
@@ -7,3 +9,9 @@ class SolicitudServicioAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'empresa', 'tipo_servicio', 'fecha_solicitud', 'estado')
     list_filter = ('tipo_servicio', 'estado', 'fecha_solicitud')
     search_fields = ('nombre', 'empresa', 'email')
+
+
+
+admin.site.register(PortfolioProject)
+
+
